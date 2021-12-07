5159377e916c39e35aa177f15e060e54051344ea
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2021-12-07 13:40:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-var slide = document.getElementById('slide');
-var up = document.getElementById('up');
-var down = document.getElementById('down');
+const box2 = document.getElementById('form_box')
+const box1 = document.getElementById('form_box_log')
+const sign = document.getElementById('sign')
+const log = document.getElementById('log')
+const close1 = document.getElementById('close1')
+const close2 = document.getElementById('close2')
+const already = document.getElementById('already')
+const register = document.getElementById('register')
 
-let x = 0;
 
-up.onclick = ()=>{
-    if(x > -900){
-        x = x - 300;
-        slide.style.top = x + "px";
-    }
+log.addEventListener('click', ()=>{
+    box1.style.display = 'flex';
+})
 
-}
+close1.addEventListener('click', ()=>{
+    box1.style.display = 'none';
+})
 
-down.onclick = ()=>{
-    if(x < 0){
-        x = x + 300;
-        slide.style.top = x + "px";
-    }
-}
+sign.addEventListener('click', ()=>{
+    box2.style.display = 'flex';
+})
+
+close2.addEventListener('click', ()=>{
+    box2.style.display = 'none';
+})
+
+already.addEventListener('click', (e)=>{
+    box2.style.display = 'none';
+    box1.style.display = 'flex';
+})
+
+register.addEventListener('click', (e)=>{
+    box1.style.display = 'none';
+    box2.style.display = 'flex';
+})
+
